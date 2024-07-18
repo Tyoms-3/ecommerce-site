0558f776e622b27ce4a00b1821198d1de268e897
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-18 15:22:30 +0200

    Update background et bandeau index.js

--- my-nextjs-app/pages/index.js
+++ my-nextjs-app/pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import { ChakraProvider, Box, Text } from '@chakra-ui/react'
+import Head from 'next/head';
+import { ChakraProvider, Box, Text } from '@chakra-ui/react';
 
 export default function Home() {
   return (
@@ -8,11 +8,39 @@ export default function Home() {
         <Head>
           <title>OneTMD - E-commerce</title>
         </Head>
+        {/* Bandeau */}
+        <Box className="bandeau">
+          <img src="/logo.png" alt="Logo One TMD" />
+          <Text fontSize="xl" fontWeight="bold">One TMD</Text>
+        </Box>
+        {/* Contenu principal */}
         <Box p={5}>
           <Text fontSize="xl">Welcome to OneTMD E-commerce!</Text>
-          {/* Ajoutez le contenu de votre site ici */}
+          {/* Image de fond */}
+          <Box
+            className="background-image"
+            h="300px"
+            backgroundImage="url('/background-image.jpg')" // Remplacez par votre image de fond
+            backgroundSize="cover"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+            mb={5}
+          >
+            <Box bg="rgba(0,0,0,0.5)" h="100%">
+              <Text fontSize="2xl" color="white" fontWeight="bold" textAlign="center" mt="40%">
+                L'attention à portée de main
+              </Text>
+              <Text fontSize="lg" color="white" textAlign="center">
+                Exprimez votre amour en offrant et partageant des moments précieux et uniques où chaque détail compte.
+              </Text>
+            </Box>
+          </Box>
+          {/* Autre contenu de votre site */}
+          <Text fontSize="lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo sagittis ipsum eu hendrerit. Nulla in ligula in arcu consequat ultricies. Donec vitae nisl aliquam, aliquet neque vel, condimentum lectus. Morbi varius purus ac erat vehicula dictum. Nulla facilisi.
+          </Text>
         </Box>
       </Box>
     </ChakraProvider>
-  )
+  );
 }
