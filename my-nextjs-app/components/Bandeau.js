458e3553c485a444931f4eed6cb60db5747da370
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-19 13:42:36 +0200

    Update Bandeau.js
    retester intégration direct depuis global css

--- my-nextjs-app/components/Bandeau.js
+++ my-nextjs-app/components/Bandeau.js
@@ -1,15 +1,16 @@
 // components/Bandeau.js
 
 import React from 'react';
-import styles from '../styles/Bandeau.module.css'; // Utilisez un module CSS pour les styles locaux
+import '../styles/globals.css'; // Assurez-vous que le chemin est correct
 
 const Bandeau = () => {
     return (
-        <div className={styles.bandeau}>
-            <img src="/logo.png" alt="Logo One TMD" className={styles.logo} />
+        <div className="bandeau"> {/* Utilisation des classes CSS globales */}
+            <img src="/logo.png" alt="Logo One TMD" className="logo" />
             <h1>One TMD</h1>
         </div>
     );
 };
 
 export default Bandeau;
+
