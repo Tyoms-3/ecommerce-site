ecbb8f18388e7597cc2a48439457bd9dd5195fea
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-24 09:23:35 +0200

    Update Bandeau.js

--- my-nextjs-app/components/Bandeau.js
+++ my-nextjs-app/components/Bandeau.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Headroom from 'react-headroom';
 import {
   Box,
   Flex,
@@ -20,40 +21,37 @@ const Bandeau = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Box className="imageFond">
-      <Flex justify="space-between" align="center" w="100%" p={4} color="white">
-        <IconButton
-          aria-label="Open menu"
-          icon={<HamburgerIcon />}
-          variant="outline"
-          colorScheme="whiteAlpha"
-          onClick={onOpen}
-        />
-        <Heading as="h1" size="lg" className="titrePrincipal">
-          One TMD
-        </Heading>
-        <img src="/logo.png" alt="Logo One TMD" className="logo" />
-      </Flex>
+    <Headroom>
+      <Box className={styles.bandeau} bg="black" color="white" p={4}>
+        <Flex justify="space-between" align="center" w="100%">
+          <IconButton
+            aria-label="Open menu"
+            icon={<HamburgerIcon />}
+            variant="outline"
+            colorScheme="whiteAlpha"
+            onClick={onOpen}
+          />
+          <Heading as="h1" size="lg" className={styles.titrePrincipal}>
+            One TMD
+          </Heading>
+          <img src="/logo.png" alt="Logo One TMD" className={styles.logo} />
+        </Flex>
 
-      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerBody>
-            <Stack spacing={4} mt={8}>
-              <Text fontSize="xl">Page 1</Text>
-              <Text fontSize="xl">Page 2</Text>
-              <Text fontSize="xl">Page 3</Text>
-            </Stack>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-
-      <div className="contenuTexte">
-        <Heading className="titrePrincipal">Bienvenue sur One TMD</Heading>
-        <Text className="slogan">Votre slogan ici</Text>
-      </div>
-    </Box>
+        <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerBody>
+              <Stack spacing={4} mt={8}>
+                <Text fontSize="xl">Page 1</Text>
+                <Text fontSize="xl">Page 2</Text>
+                <Text fontSize="xl">Page 3</Text>
+              </Stack>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </Box>
+    </Headroom>
   );
 };
 
