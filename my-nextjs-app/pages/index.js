f7f54c277ec0804fe65ad0b355a1a62ff9c02b61
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-24 09:32:12 +0200

    Update index.js

--- my-nextjs-app/pages/index.js
+++ my-nextjs-app/pages/index.js
@@ -1,5 +1,8 @@
+// pages/index.js
+
 import Head from 'next/head';
-import { ChakraProvider, Box, Text } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
+import styles from '../styles/globals.css'; // Assurez-vous que le chemin est correct
 
 export default function Home() {
   return (
@@ -10,20 +13,19 @@ export default function Home() {
       {/* Contenu principal */}
       <Box p={5}>
         <Text fontSize="xl">Bienvenu sur notre boutique !</Text>
-        {/* Contenu principal */}
-        <Box mb={5}>
-          <Text fontSize="2xl" fontWeight="bold" textAlign="center" mt={10}>
-            L'attention à portée de main
-          </Text>
-          <Text fontSize="lg" textAlign="center" mt={3}>
-            Exprimez votre amour en offrant et partageant des moments précieux et uniques où chaque détail compte.
-          </Text>
+        {/* Section avec image de fond */}
+        <Box className="sectionPrincipale">
+          <Box className="contenuTexte">
+            <Text className="titrePrincipal">L'attention à portée de main</Text>
+            <Text className="slogan">Exprimez votre amour en offrant et partageant des moments précieux et uniques où chaque détail compte.</Text>
+          </Box>
         </Box>
         {/* Autre contenu de votre site */}
-        <Text fontSize="lg">
+        <Text fontSize="lg" mt={5}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo sagittis ipsum eu hendrerit. Nulla in ligula in arcu consequat ultricies. Donec vitae nisl aliquam, aliquet neque vel, condimentum lectus. Morbi varius purus ac erat vehicula dictum. Nulla facilisi.
         </Text>
       </Box>
     </Box>
   );
 }
+
