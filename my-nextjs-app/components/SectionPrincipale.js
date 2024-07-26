055e34505a7f18c52f5783f2c5cf9604fd629308
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-26 14:25:45 +0200

    Update SectionPrincipale.js

--- my-nextjs-app/components/SectionPrincipale.js
+++ my-nextjs-app/components/SectionPrincipale.js
@@ -6,15 +6,13 @@ import styles from '../styles/globals.css';
 const SectionPrincipale = () => {
     return (
         <section className={styles.sectionPrincipale}>
-            <div className={styles.imageFond}>
-                <img src="/image-fond.jpg" alt="Image de fond" />
-                <div className={styles.contenuTexte}>
-                    <h2 className={styles.titrePrincipal}>L'attention à portée de main</h2>
-                    <p className={styles.slogan}>Exprimez votre amour en offrant et partageant des moments précieux et uniques où chaque détail compte.</p>
-                </div>
+            <div className={styles.contenuTexte}>
+                <h2 className={styles.titrePrincipal}>L'attention à portée de main</h2>
+                <p className={styles.slogan}>Exprimez votre amour en offrant et partageant des moments précieux et uniques où chaque détail compte.</p>
             </div>
         </section>
     );
 };
 
 export default SectionPrincipale;
+
