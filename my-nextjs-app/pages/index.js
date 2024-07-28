b0faa15eb5d953ada9bea3dda70d42f06b87461e
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-28 20:10:49 +0200

    Update index.js

--- my-nextjs-app/pages/index.js
+++ my-nextjs-app/pages/index.js
@@ -39,29 +39,6 @@ export default function Home() {
             <p className="price">€300</p>
           </Box>
         </Box>
-
-        {/* Autre contenu de votre site */}
-        <Box id="products">
-          <Box className="product">
-            <img src="/path/to/image4.jpg" alt="Product 4" />
-            <h3>Produit 4</h3>
-            <p>Description du produit 4.</p>
-            <p className="price">€400</p>
-          </Box>
-          <Box className="product">
-            <img src="/path/to/image5.jpg" alt="Product 5" />
-            <h3>Produit 5</h3>
-            <p>Description du produit 5.</p>
-            <p className="price">€500</p>
-          </Box>
-          <Box className="product">
-            <img src="/path/to/image6.jpg" alt="Product 6" />
-            <h3>Produit 6</h3>
-            <p>Description du produit 6.</p>
-            <p className="price">€600</p>
-          </Box>
-        </Box>
-      </Box>
     </Box>
   );
 }
