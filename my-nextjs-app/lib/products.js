ccd5b8e189980af94210054698803600de3d06af
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-30 14:52:11 +0200

    Update products.js

--- my-nextjs-app/lib/products.js
+++ my-nextjs-app/lib/products.js
@@ -2,15 +2,15 @@
 
 // Simule des données de produits
 const products = [
-  { id: 1, name: 'T-shirt Personnalisé', price: 20, imageUrl: '/image3-fond.jpg' },
-  { id: 2, name: 'Sweat-shirt Personnalisé', price: 30, imageUrl: '/image3-fond.jpg' },
-  // Ajoutez d'autres produits ici
-];
+  { id: 1, name: 'Sweat personnalisé', price: 20, imageUrl: '/image1.jpg' },
+  { id: 2, name: 'Pull personnalisé', price: 30, imageUrl: '/image2.jpg' },
+  { id: 3, name: 'Pyjama personnalisé', price: 25, imageUrl: '/image3.jpg' }
+]
 
 export const getProductById = (id) => {
   return products.find((product) => product.id === parseInt(id, 10));
-};
+}
 
 export const getAllProducts = () => {
   return products;
-};
+}
