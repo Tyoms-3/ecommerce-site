51bd5f229888628fe47317311900e4c6c728f8b2
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-30 13:22:01 +0200

    Update [id].js

--- my-nextjs-app/pages/api/products/[id].js
+++ my-nextjs-app/pages/api/products/[id].js
@@ -1,24 +1,13 @@
 // pages/api/products/[id].js
-
-// Exemple de données statiques, remplacez par vos données réelles
-const products = [
-  { id: 1, name: 'Sweat Personnalisé', price: 20, imageUrl: '/image3-fond.jpg' },
-  { id: 2, name: 'Pyjama Personnalisé', price: 25, imageUrl: '/image2.jpg' },
-  // Ajoutez d'autres produits ici
-];
+import { products } from '../../../data/products'; // Assurez-vous d'avoir un fichier `data/products.js`
 
 export default function handler(req, res) {
   const { id } = req.query;
+  const product = products.find((p) => p.id === parseInt(id, 10));
 
-  if (req.method === 'GET') {
-    const product = products.find(p => p.id === parseInt(id));
-    if (product) {
-      res.status(200).json(product);
-    } else {
-      res.status(404).json({ message: 'Product not found' });
-    }
+  if (product) {
+    res.status(200).json(product);
   } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    res.status(404).json({ message: 'Product not found' });
   }
 }
