b9d9f75553951cdd5346c5a25c6568c021319ea8
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-30 13:45:11 +0200

    Update [id].js

--- my-nextjs-app/pages/api/products/[id].js
+++ my-nextjs-app/pages/api/products/[id].js
@@ -1,13 +1,9 @@
 // pages/api/products/[id].js
-import fs from 'fs';
-import path from 'path';
+import { getProductById } from '../../../lib/products';
 
 export default function handler(req, res) {
   const { id } = req.query;
-  const filePath = path.join(process.cwd(), 'public/products.json');
-  const jsonData = fs.readFileSync(filePath);
-  const products = JSON.parse(jsonData);
-  const product = products.find(product => product.id === id);
+  const product = getProductById(id);
 
   if (product) {
     res.status(200).json(product);
