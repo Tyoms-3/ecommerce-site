c9598fc56b9d9d6e3ec49b74ecee24851b1f8c31
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-30 13:21:44 +0200

    Update index.js

--- my-nextjs-app/pages/api/products/index.js
+++ my-nextjs-app/pages/api/products/index.js
@@ -1,18 +1,6 @@
 // pages/api/products/index.js
-
-// Exemple de données statiques, remplacez par vos données réelles
-const products = [
-  { id: 1, name: 'Sweat Personnalisé', price: 20, imageUrl: '/image3-fond.jpg' },
-  { id: 2, name: 'Pyjama Personnalisé', price: 25, imageUrl: '/image2.jpg' },
-  // Ajoutez d'autres produits ici
-];
+import { products } from '../../../data/products'; // Assurez-vous d'avoir un fichier `data/products.js`
 
 export default function handler(req, res) {
-  if (req.method === 'GET') {
-    // Renvoyer la liste des produits
-    res.status(200).json(products);
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+  res.status(200).json(products);
 }
