c82f18dcd13cf95e72baafb533e7c7b8ceba45a6
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-30 15:00:43 +0200

    Update [id].js

--- my-nextjs-app/pages/api/products/[id].js
+++ my-nextjs-app/pages/api/products/[id].js
@@ -3,7 +3,14 @@ import { getProductById } from '../../../lib/products';
 
 export default function handler(req, res) {
   const { id } = req.query;
+
+  // Assurez-vous que l'ID est bien un nombre
+  if (!id || isNaN(id)) {
+    return res.status(400).json({ message: 'Invalid product ID' });
+  }
+
   console.log('Requested product ID:', id); // Debugging: Check the requested ID
+
   const product = getProductById(id);
 
   if (product) {
