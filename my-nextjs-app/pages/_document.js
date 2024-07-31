0de5bbe68b31fa8dc6cb11f412dcf1dd40f3a286
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-31 10:42:22 +0200

    Update and rename document.js to _document.js

--- my-nextjs-app/pages/_document.js
+++ my-nextjs-app/pages/_document.js
@@ -1,14 +1,16 @@
 // pages/_document.js
+// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="fr"> {/* Définir la langue par défaut ici */}
+      <Html lang="fr"> {/* Langue de la page */}
         <Head>
           {/* Favicon */}
           <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-          {/* Icons for various devices and browsers */}
+
+          {/* Apple Touch Icons */}
           <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
           <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
           <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
@@ -18,14 +20,22 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
           <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+
+          {/* Android Icons */}
           <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+
+          {/* Favicon Sizes */}
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+          {/* Manifest */}
           <link rel="manifest" href="/manifest.json" />
+
+          {/* MS Icons */}
+          <meta name="msapplication-config" content="/browserconfig.xml" />
           <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-          <meta name="theme-color" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/ms-icon-150x150.png" />
         </Head>
         <body>
           <Main />
