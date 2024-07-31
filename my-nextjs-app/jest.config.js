5ef2a702b71567c92a64742d183f490940192bc6
Tyoms-3 <tyoms.dp@gmail.com>
2024-07-31 23:07:10 +0200

    Create jest.config.js

---
+++ my-nextjs-app/jest.config.js
@@ -0,0 +1,10 @@
+// jest.config.js
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
