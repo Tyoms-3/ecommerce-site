88cfc1cb1972575e24df9f813d98a3f55403eb45
Tyoms-3 <tyoms.dp@gmail.com>
2024-08-03 15:23:18 +0200

    Update [id].js

--- my-nextjs-app/pages/api/products/[id].js
+++ my-nextjs-app/pages/api/products/[id].js
@@ -1,9 +1,19 @@
-//pages/api/product/[id].js
+// pages/api/product/[id].js
 import { getProductById } from '../../../lib/products';
 
 export default function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
   const { id } = req.query;
+
+  if (!id || typeof id !== 'string') {
+    return res.status(400).json({ message: 'Invalid ID' });
+  }
+
   console.log('Requested product ID:', id); // Affiche l'ID demandé
+
   const product = getProductById(id);
 
   if (product) {
