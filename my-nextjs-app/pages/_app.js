01bf035eed503dd6a914a716cc7c84553ddb6193
Tyoms-3 <tyoms.dp@gmail.com>
2024-08-03 15:18:07 +0200

    Update _app.js

--- my-nextjs-app/pages/_app.js
+++ my-nextjs-app/pages/_app.js
@@ -1,16 +1,38 @@
 // pages/_app.js
+import { ChakraProvider } from '@chakra-ui/react';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import Head from 'next/head';
+import { extendTheme } from '@chakra-ui/react';
 
-import { ChakraProvider, Box } from '@chakra-ui/react';
-import '../styles/globals.css';
-import Bandeau from '../components/Bandeau';
+// Configuration du thème Chakra UI
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: '#f7c6c7',
+      900: '#1a202c',
+    },
+  },
+  fonts: {
+    heading: 'Arial, sans-serif',
+    body: 'Arial, sans-serif',
+  },
+});
+
+const initialOptions = {
+  clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+  currency: "EUR",
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Bandeau />
-      <Box mt="0px"> {/* Marge en haut pour éviter que le contenu soit masqué */}
+    <ChakraProvider theme={theme}>
+      <PayPalScriptProvider options={initialOptions}>
+        <Head>
+          <title>One TMD</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
         <Component {...pageProps} />
-      </Box>
+      </PayPalScriptProvider>
     </ChakraProvider>
   );
 }
