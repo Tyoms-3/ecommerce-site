6902bb6de49b534c9249a613cf121e0e2db91cea
Tyoms-3 <tyoms.dp@gmail.com>
2024-08-04 15:16:43 +0200

    Update _app.js

--- my-nextjs-app/pages/_app.js
+++ my-nextjs-app/pages/_app.js
@@ -4,7 +4,6 @@ import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import Head from 'next/head';
 import { extendTheme } from '@chakra-ui/react';
 import '../styles/globals.css';  // Import des styles globaux
-import '../styles/styles.css';  // Import des styles spécifiques
 
 // Configuration du thème Chakra UI
 const theme = extendTheme({
