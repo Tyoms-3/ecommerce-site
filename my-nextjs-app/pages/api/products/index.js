cc89ce6a00c19d62d07936a033035ca8a64bcfbf
Tyoms-3 <tyoms.dp@gmail.com>
2024-08-06 11:27:50 +0200

    Update index.js

--- my-nextjs-app/pages/api/products/index.js
+++ my-nextjs-app/pages/api/products/index.js
@@ -1,10 +1,15 @@
 // pages/api/products/index.js
-import { getAllProducts } from '../../../lib/products';
+import clientPromise from '../../../lib/mongodb';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
-      const products = getAllProducts();
+      const client = await clientPromise;
+      const db = client.db('Project 0'); // Remplacez 'myDatabase' par le nom réel de votre base de données
+      const collection = db.collection('products');
+
+      const products = await collection.find({}).toArray();
+
       res.status(200).json(products);
     } catch (error) {
       console.error('Error fetching products:', error); // Log the error if something goes wrong
