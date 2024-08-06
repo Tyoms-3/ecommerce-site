8c3e76e3f9138b231519e8c73e11a9edfb96817f
Tyoms-3 <tyoms.dp@gmail.com>
2024-08-06 11:24:24 +0200

    Update [id].js

--- my-nextjs-app/pages/api/products/[id].js
+++ my-nextjs-app/pages/api/products/[id].js
@@ -1,21 +1,24 @@
 // pages/api/products/[id].js
 
-import clientPromise from '../../../lib/mongodb';
+import clientPromise from '../../../lib/mongodb'; // Assurez-vous que ce fichier est correctement configuré
+import { ObjectId } from 'mongodb'; // Importez ObjectId depuis 'mongodb' A VERIFIER 
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
       const { id } = req.query;
 
+      // Validation de l'ID
       if (!id || typeof id !== 'string') {
         return res.status(400).json({ message: 'Invalid ID' });
       }
 
       const client = await clientPromise;
-      const db = client.db('yourDatabaseName'); // Remplacez par le nom de votre base de données
+      const db = client.db('Project 0'); // Remplacez par le nom de votre base de données
       const collection = db.collection('products');
 
-      const product = await collection.findOne({ _id: new MongoClient.ObjectId(id) });
+      // Requête pour trouver le produit par ID
+      const product = await collection.findOne({ _id: new ObjectId(id) });
 
       if (product) {
         res.status(200).json(product);
