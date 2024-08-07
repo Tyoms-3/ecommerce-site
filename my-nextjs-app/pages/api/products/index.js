e4c3447cf947436a397f26fd45b91844a8097fa4
Tyoms-3 <tyoms.dp@gmail.com>
2024-08-07 12:17:24 +0200

    Update index.js

--- my-nextjs-app/pages/api/products/index.js
+++ my-nextjs-app/pages/api/products/index.js
@@ -1,23 +1,22 @@
 // pages/api/products/index.js
-import clientPromise from '../../lib/mongodb'; // Assurez-vous que le chemin est correct
-
 export default async function handler(req, res) {
-  const client = await clientPromise;
-  const db = client.db('ecommerce'); // Remplacez par le nom de votre base de données
-  const collection = db.collection('products');
-
   if (req.method === 'POST') {
     try {
-      const bodyObject = JSON.parse(req.body);
-      const newProduct = await collection.insertOne(bodyObject);
-      res.status(201).json(newProduct.ops[0]);
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/.netlify/functions/products`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(req.body),
+      });
+      const result = await response.json();
+      res.status(201).json(result);
     } catch (error) {
       console.error('Error creating product:', error);
       res.status(500).json({ message: 'Internal Server Error' });
     }
   } else if (req.method === 'GET') {
     try {
-      const products = await collection.find({}).toArray();
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/.netlify/functions/products`);
+      const products = await response.json();
       res.status(200).json(products);
     } catch (error) {
       console.error('Error fetching products:', error);
