c02f3d2e32ce9a663b25f8d57e47e44dd2290b98
Tyoms-3 <tyoms.dp@gmail.com>
2024-08-07 12:17:43 +0200

    Update cart.js

--- my-nextjs-app/functions/cart.js
+++ my-nextjs-app/functions/cart.js
@@ -1,13 +1,10 @@
   // functions/cart.js
-
-const { MongoClient } = require('mongodb');
-
-const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+import clientPromise from '../lib/mongodb.js';
 
 exports.handler = async (event, context) => {
   try {
-    await client.connect();
-    const db = client.db('ecommerce'); // Remplacez par le nom de votre base de données
+    const client = await clientPromise;
+    const db = client.db('ecommerce');
     const collection = db.collection('cart');
 
     if (event.httpMethod === 'GET') {
@@ -17,15 +14,13 @@ exports.handler = async (event, context) => {
         body: JSON.stringify(cartItems),
       };
     } else if (event.httpMethod === 'POST') {
-      const item = JSON.parse(event.body);
-
+      const { item } = JSON.parse(event.body);
       if (!item) {
         return {
           statusCode: 400,
           body: JSON.stringify({ message: 'Bad Request: Missing item' }),
         };
       }
-
       await collection.insertOne(item);
       return {
         statusCode: 201,
@@ -43,7 +38,5 @@ exports.handler = async (event, context) => {
       statusCode: 500,
       body: JSON.stringify({ message: 'Internal Server Error' }),
     };
-  } finally {
-    await client.close();
   }
 };
