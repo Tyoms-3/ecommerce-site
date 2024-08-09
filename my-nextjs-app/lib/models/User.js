44fd493f117852cdaa72e6e30476b6187745146f
Tyoms-3 <tyoms.dp@gmail.com>
2024-08-09 10:00:26 +0200

    Create User.js

---
+++ my-nextjs-app/lib/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    maxlength: [60, 'Name cannot be more than 60 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      'Please provide a valid email address',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: [6, 'Password must be at least 6 characters long'],
+    select: false, // This ensures the password is not returned in queries by default
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  location: {
+    type: String,
+    default: '',
+  },
+});
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
