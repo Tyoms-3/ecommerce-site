4bfd2a43de8b1954a93007577b7b68f1244e4c29
Tyoms-3 <tyoms.dp@gmail.com>
2024-11-05 11:18:45 +0100

    Update Cart.js

--- my-nextjs-app/lib/models/Cart.js
+++ my-nextjs-app/lib/models/Cart.js
@@ -4,35 +4,54 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const CartSchema = new Schema({
-  productId: {
+  userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Product',
+    ref: 'User',
     required: true,
   },
-  quantity: {
+  items: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      productName: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      customizations: {
+        embroidery: { type: String, default: '' },
+        price: { type: Number, default: 0 },
+      },
+      totalItemPrice: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalCartPrice: {
     type: Number,
     required: true,
-  },
-  productName: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  customizations: {
-    embroidery: { type: String, default: '' },
-    price: { type: Number, default: 0 },
-  },
-  totalItemPrice: {
-    type: Number,
-    required: true,
+    default: 0,
   },
   createdAt: {
     type: Date,
     default: Date.now,
   },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
+// Exporter le modèle Cart
 export default mongoose.models.Cart || mongoose.model('Cart', CartSchema);
