e082c4346969c627b5c36a62bbb614ab8bc04075
Tyoms-3 <tyoms.dp@gmail.com>
2024-11-05 11:07:40 +0100

    Update dbConnect.js

--- my-nextjs-app/lib/dbConnect.js
+++ my-nextjs-app/lib/dbConnect.js
@@ -1,36 +1,43 @@
 // lib/dbConnect.js
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const dbURIs = {
+  users: process.env.MONGODB_URI_USERS,
+  products: process.env.MONGODB_URI_PRODUCTS,
+  orders: process.env.MONGODB_URI_ORDERS,
+  carts: process.env.MONGODB_URI_CARTS,
+};
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
+// Vérification que chaque URI existe dans .env.local
+for (const [dbName, uri] of Object.entries(dbURIs)) {
+  if (!uri) {
+    throw new Error(`Please define the ${dbName.toUpperCase()}_URI environment variable inside .env.local`);
   }
+}
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false, // Désactive le buffering dans Mongoose
-    }).then((mongoose) => {
-      return mongoose;
+// Cache pour stocker les connexions
+const cachedConnections = global.mongooseConnections || {};
+
+global.mongooseConnections = cachedConnections;
+
+async function dbConnect(dbName) {
+  if (!cachedConnections[dbName]) {
+    const dbURI = dbURIs[dbName];
+    if (!dbURI) {
+      throw new Error(`No URI found for the database: ${dbName}`);
+    }
+
+    cachedConnections[dbName] = mongoose.createConnection(dbURI, {
+      bufferCommands: false,
+    }).then((connection) => {
+      console.log(`Connected to MongoDB database: ${dbName}`);
+      return connection;
     }).catch(err => {
-      console.error('Error connecting to MongoDB:', err);
-      throw err; // Lance l'erreur pour qu'elle soit gérée par l'appelant
+      console.error(`Error connecting to MongoDB database: ${dbName}`, err);
+      throw err;
     });
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
+  return cachedConnections[dbName];
 }
 
 export default dbConnect;
-
